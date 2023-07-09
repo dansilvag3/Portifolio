d61844d3af2b62f2e2e871951f50055c4aa6516b
Danilo Araujo <dansilvag3@hotmail.com>
2023-07-08 22:18:48 -0300

    atualizaçao 0.7

--- Sistema/js/valida.js
+++ Sistema/js/valida.js
@@ -7,5 +7,35 @@ const usuarios = [
     {
         login: 'danilo.araujo',
         pass:  'webdesign2012'
+    },
+
+    {
+        login: 'visitante',
+        pass:  'webdesign2019'
     }
 ]
+
+let botao = document.getElementById('btnlogar')
+botao.addEventListener('click', function logar(){
+    let pegaUsuario = document.getElementById('username').value
+    let pegaSenha = document.getElementById('password').value
+    let validaLogin = false
+
+    for(let i in usuarios){
+        
+        if(pegaUsuario == usuarios[i].login && pegaSenha == usuarios[i].pass){
+            validaLogin = true
+            break
+        }
+
+    }
+
+        if(validaLogin == true){
+        window.location = "index.html"
+        }
+
+        else{
+        alert('Usuário ou Senha Inválido')
+    }
+
+})
